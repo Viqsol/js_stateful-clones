de41d542a9766d04a05432f5310c2dcb230e681c
Viktor Gulko <viq@ukr.net>
2026-04-02 18:41:39 +0300

    fixed mistakes showed by Viktoria

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,23 +11,46 @@ function transformStateWithClones(state, actions) {
   const copyState = Object.assign({}, state);
 
   for (const action of actions) {
-    if (action.type === 'addProperties') {
-      for (const key in action.extraData) {
-        copyState[key] = action.extraData[key];
-      }
+    switch (action.type) {
+      case 'addProperties':
+        for (const key in action.extraData) {
+          copyState[key] = action.extraData[key];
+        }
+        break;
+
+      case 'removeProperties':
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+        break;
+
+      case 'clear':
+        for (const key in copyState) {
+          delete copyState[key];
+        }
+        break;
+
+      default:
+        throw new Error('Unknown action type');
     }
 
-    if (action.type === 'removeProperties') {
-      for (const key of action.keysToRemove) {
-        delete copyState[key];
-      }
-    }
+    // if (action.type === 'addProperties') {
+    //   for (const key in action.extraData) {
+    //     copyState[key] = action.extraData[key];
+    //   }
+    // }
 
-    if (action.type === 'clear') {
-      for (const key in copyState) {
-        delete copyState[key];
-      }
-    }
+    // if (action.type === 'removeProperties') {
+    //   for (const key of action.keysToRemove) {
+    //     delete copyState[key];
+    //   }
+    // }
+
+    // if (action.type === 'clear') {
+    //   for (const key in copyState) {
+    //     delete copyState[key];
+    //   }
+    // }
     stateHistory.push(Object.assign({}, copyState));
   }
 
