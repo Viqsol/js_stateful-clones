195ab8d7f6a2bbd7a36323e53239e9f4375398a3
Viktor Gulko <viq@ukr.net>
2026-04-02 16:48:20 +0300

    first attempt

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,31 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const stateHistory = [];
+  const copyState = Object.assign({}, state);
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      for (const key in action.extraData) {
+        copyState[key] = action.extraData[key];
+      }
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete copyState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      for (const key in copyState) {
+        delete copyState[key];
+      }
+    }
+    stateHistory.push(Object.assign({}, copyState));
+  }
+
+  return stateHistory;
 }
 
 module.exports = transformStateWithClones;
